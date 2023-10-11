1b27f92abfbd8b70ecbb6dec2cf38397c1e3d73c
rodrigototy <rodrigototy@yahoo.com.br>
2023-10-11 11:45:33 -0300

    Ajuste field imageSrc in listaProdutos

--- backend/catalogo-produtos/script.js
+++ backend/catalogo-produtos/script.js
@@ -2,7 +2,7 @@ const listaProdutos = [
   {
     id: 1,
     imageSrc:
-      "https://github.com/rodrigototy/talento-cloud-pro/tree/main/repository/images/item-001-patinete.webp",
+      "https://rodrigototy.github.io/talento-cloud-pro/repository/images/item-001-patinete.webp",
     nomeItem: "Patinete Scooter Dobrável",
     descreveItem:
       "Patinete Scooter dobrável com freio modelo Bv008 na cor azul",
@@ -11,7 +11,7 @@ const listaProdutos = [
   {
     id: 2,
     imageSrc:
-      "https://github.com/rodrigototy/talento-cloud-pro/tree/main/repository/images/item-002-bicicleta.webp",
+      "https://rodrigototy.github.io/talento-cloud-pro/repository/images/item-002-bicicleta.webp",
     nomeItem: "Bicicleta Infantil Spider",
     descreveItem: "Bicicleta infantil menino Spider Marvel Aro 12 Homem Aranha",
     precoItem: "R$ 350,00",
@@ -19,7 +19,7 @@ const listaProdutos = [
   {
     id: 3,
     imageSrc:
-      "https://github.com/rodrigototy/talento-cloud-pro/tree/main/repository/images/item-003-jenga.webp",
+      "https://rodrigototy.github.io/talento-cloud-pro/repository/images/item-003-jenga.webp",
     nomeItem: "Jenga Clássico Hasbro",
     descreveItem: "Jenga Clássico Hasbro modelo A2120",
     precoItem: "R$ 89,00",
@@ -27,7 +27,7 @@ const listaProdutos = [
   {
     id: 4,
     imageSrc:
-      "https://github.com/rodrigototy/talento-cloud-pro/tree/main/repository/images/item-004-cara-cara.webp",
+      "https://rodrigototy.github.io/talento-cloud-pro/repository/images/item-004-cara-cara.webp",
     nomeItem: "Jogo Cara a Cara",
     descreveItem: "Jogo de mesa Cara a cara Com aplicativo Estrela",
     precoItem: "R$ 98,79",
