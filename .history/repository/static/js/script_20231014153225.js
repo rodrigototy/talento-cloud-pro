ed27481b31b1c48b3aa517241772ed5fc570c0ae
rodrigototy <rodrigototy@yahoo.com.br>
2023-10-14 16:13:59 -0300

    Update Homepage for Projects

---
+++ .history/repository/static/js/script_20231014153225.js
@@ -0,0 +1,27 @@
+import databaseProject from "./databaseProject.js";
+
+const urlPageProject = "https://rodrigototy.github.io/talento-cloud-pro/";
+const urlRepositoryProject =
+  "https://github.com/rodrigototy/talento-cloud-pro/tree/main/";
+  
+const columnIndex = document.querySelector(".columnIndex");
+const columnHomepage = document.querySelector(".columnHomepage");
+const columnRepository = document.querySelector(".columnRepository");
+const columnDescricao = document.querySelector(".columnDescricao");
+
+databaseProject.forEach((element) => {
+  const itemIndex = document.createElement("p");
+  const itemHomepage = document.createElement("p");
+  const itemRepository = document.createElement("p");
+  const itemDescricao = document.createElement("p");
+
+  itemIndex.innerHTML = `${element.index}`;
+  itemHomepage.innerHTML = `<a href=${urlPageProject}${element.pasta}>${element.nome}</a></>`;
+  itemRepository.innerHTML = `<p><a href=${urlRepositoryProject}${element.pasta}>${element.nome}</a></p>`;
+  itemDescricao.innerHTML = `<p>${element.descricao}</p>`;
+
+  columnIndex.appendChild(itemIndex);
+  columnHomepage.appendChild(itemHomepage);
+  columnRepository.appendChild(itemRepository);
+  columnDescricao.appendChild(itemDescricao);
+});
