b15b0493c35fd39d11c9771bcbc1eace5ccc620d
rodrigototy <rodrigototy@yahoo.com.br>
2023-10-18 20:48:33 -0300

    Ajuste msg Helper for Validação Formulários

--- js-2/validacao-formularios/static/js/script.js
+++ js-2/validacao-formularios/static/js/script.js
@@ -5,19 +5,22 @@ let usernameHelper = document.getElementById("username-helper");
 
 // Mostrar popup de campo obrigatório
 usernameInput.addEventListener("focus", () => {
-    usernameLabel.classList.add("required-popup");
+  usernameLabel.classList.add("required-popup");
 });
 
 // Ocultar popup de campo obrigatório
 usernameInput.addEventListener("blur", (e) => {
-    if (e.target.value.length > 5) {
-        usernameLabel.classList.remove("required-popup");
-        usernameInput.classList.remove("error")
-        usernameInput.classList.add("correct")
-    } else {
-        usernameInput.classList.remove("correct")
-        usernameInput.classList.add("error")
-    }
+  if (e.target.value.length > 5) {
+    usernameLabel.classList.remove("required-popup");
+    usernameInput.classList.remove("error");
+    usernameInput.classList.add("correct");
+    usernameHelper.classList.remove("visible");
+  } else {
+    usernameInput.classList.remove("correct");
+    usernameInput.classList.add("error");
+    usernameHelper.classList.add("visible");
+    usernameHelper.innerHTML = "O username deve ter pelo menos 6 caracteres";
+  }
 });
 
 // Validar valor do input
