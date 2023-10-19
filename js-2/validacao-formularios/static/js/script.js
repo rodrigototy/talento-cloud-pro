6045e34077d220f7f95c4be0c4701b2b529f25d8
rodrigototy <rodrigototy@yahoo.com.br>
2023-10-18 21:17:21 -0300

    Include function for validation fields

--- js-2/validacao-formularios/static/js/script.js
+++ js-2/validacao-formularios/static/js/script.js
@@ -8,9 +8,29 @@ const emailLabel = document.querySelector('label[for="email"]');
 const emailHelper = document.getElementById("email-helper");
 
 function validarEmail(email) {
-    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return regexEmail.test(email);
+  const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regexEmail.test(email);
+}
+
+function formatarCampo(
+  status,
+  objetoLabel,
+  objetoInput,
+  objetoHelper,
+  mensagemAjuda
+) {
+  if (status) {
+    objetoLabel.classList.remove("required-popup");
+    objetoInput.classList.remove("error");
+    objetoInput.classList.add("correct");
+    objetoHelper.classList.remove("visible");
+  } else {
+    objetoInput.classList.remove("correct");
+    objetoInput.classList.add("error");
+    objetoHelper.classList.add("visible");
+    objetoHelper.innerHTML = mensagemAjuda;
   }
+}
 
 // Mostrar popup de campo obrigatório
 usernameInput.addEventListener("focus", () => {
@@ -18,36 +38,26 @@ usernameInput.addEventListener("focus", () => {
 });
 
 emailInput.addEventListener("focus", () => {
-    emailLabel.classList.add("required-popup");
-  });
-  
+  emailLabel.classList.add("required-popup");
+});
+
 // Validar valor do input
 usernameInput.addEventListener("blur", (e) => {
-  if (e.target.value.length > 5) {
-    usernameLabel.classList.remove("required-popup");
-    usernameInput.classList.remove("error");
-    usernameInput.classList.add("correct");
-    usernameHelper.classList.remove("visible");
-  } else {
-    usernameInput.classList.remove("correct");
-    usernameInput.classList.add("error");
-    usernameHelper.classList.add("visible");
-    usernameHelper.innerHTML = "O username deve ter pelo menos 6 caracteres";
-  }
+  formatarCampo(
+    e.target.value.length > 5,
+    usernameLabel,
+    usernameInput,
+    usernameHelper,
+    "O username deve ter pelo menos 6 caracteres"
+  );
 });
 
 emailInput.addEventListener("blur", (e) => {
-    if (validarEmail(e.target.value)) {
-        emailLabel.classList.remove("required-popup");
-        emailInput.classList.remove("error");
-        emailInput.classList.add("correct");
-        emailHelper.classList.remove("visible");
-    } else {
-        emailInput.classList.remove("correct");
-        emailInput.classList.add("error");
-        emailHelper.classList.add("visible");
-        emailHelper.innerHTML = "Digite um email válido";
-    }
-  });
-  
-
+  formatarCampo(
+    validarEmail(e.target.value),
+    emailLabel,
+    emailInput,
+    emailHelper,
+    "Digite um email válido"
+  );
+});
