e1f57e7a85bf9f8de0fffb7112f547b488b02585
rodrigototy <rodrigototy@yahoo.com.br>
2023-10-22 22:34:32 -0300

    Re-write functions on senior level

--- js-2/validacao-formularios/static/js/script.js
+++ js-2/validacao-formularios/static/js/script.js
@@ -1,210 +1,154 @@
-// Seleção de objetos do formulário
-const msgErroUsername = "O username deve ter pelo menos 6 caracteres\n";
-const msgErroEmail = "Digite um email válido\n";
-const msgErroIdade = "A idade informada é inválida!\n";
-const msgErroSenha = "A senha não atende aos critérios de segurança.\n";
-const msgErroConfereSenha = "As senhas não conferem!";
-
-const usernameInput = document.getElementById("username");
-const usernameLabel = document.querySelector('label[for="username"]');
-const usernameHelper = document.getElementById("username-helper");
-
-const emailInput = document.getElementById("email");
-const emailLabel = document.querySelector('label[for="email"]');
-const emailHelper = document.getElementById("email-helper");
-
-const idadeInput = document.getElementById("idade");
-const idadeLabel = document.querySelector('label[for="idade"]');
-const idadeHelper = document.getElementById("idade-helper");
-
-const senhaInput = document.getElementById("senha");
-const senhaLabel = document.querySelector('label[for="senha"]');
-const senhaHelper = document.getElementById("senha-helper");
-
-const confereSenhaInput = document.getElementById("confirma-senha");
-const confereSenhaLabel = document.querySelector('label[for="confirma-senha"]');
-const confereSenhaHelper = document.getElementById("confirma-senha-helper");
+// Definir informações dos campos
+const fields = [
+  {
+    fieldName: "username",
+    requerido: true,
+    input: document.getElementById("username"),
+    label: document.querySelector('label[for="username"]'),
+    helper: document.getElementById("username-helper"),
+    errorMessage: "O username deve ter pelo menos 6 caracteres\n",
+    validate: validarUsername,
+  },
+  {
+    fieldName: "email",
+    requerido: true,
+    input: document.getElementById("email"),
+    label: document.querySelector('label[for="email"]'),
+    helper: document.getElementById("email-helper"),
+    errorMessage: "Digite um email válido\n",
+    validate: validarEmail,
+  },
+  {
+    fieldName: "idade",
+    requerido: false,
+    input: document.getElementById("idade"),
+    label: document.querySelector('label[for="idade"]'),
+    helper: document.getElementById("idade-helper"),
+    errorMessage: "A idade informada é inválida!\n",
+    validate: validarIdade,
+  },
+  {
+    fieldName: "senha",
+    requerido: true,
+    input: document.getElementById("senha"),
+    label: document.querySelector('label[for="senha"]'),
+    helper: document.getElementById("senha-helper"),
+    errorMessage: "A senha não atende aos critérios de segurança.\n",
+    validate: validarSenha,
+  },
+  {
+    fieldName: "confirma-senha",
+    requerido: true,
+    input: document.getElementById("confirma-senha"),
+    label: document.querySelector('label[for="confirma-senha"]'),
+    helper: document.getElementById("confirma-senha-helper"),
+    errorMessage: "As senhas não conferem!",
+    validate: (confirmaSenha) => confereSenha(confirmaSenha, fields.find(field => field.fieldName === "senha").input.value),
+  },
+];
 
 const buttonEnviar = document.querySelector('button[type="submit"]');
 
-buttonEnviar.addEventListener("click", (event) => {
+buttonEnviar.addEventListener("click", validarCampos);
+
+function encontrarIndiceDoCampo(fieldName) {
+  return fields.findIndex((field) => field.fieldName === fieldName);
+}
+
+function validarCampos() {
   let msgErro = "";
   let fieldFocus = "";
 
-  if (!validarUsername()) {
-    msgErro += msgErroUsername;
-    if (fieldFocus == undefined || fieldFocus == null || fieldFocus == "") {
-      fieldFocus = "usernameInput";
+  fields.forEach((field) => {
+    const isValid = field.validate(field.input.value);
+
+    if (!isValid) {
+      msgErro += field.errorMessage;
+      if (!fieldFocus) {
+        fieldFocus = field.input.id;
+      }
     }
-  }
-  if (!validarEmail(emailInput.value)) {
-    msgErro += msgErroEmail;
-    if (fieldFocus == undefined || fieldFocus == null || fieldFocus == "") {
-      fieldFocus = "emailInput";
-    }
-  }
-  if (!validarIdade(idadeInput.value)) {
-    msgErro += msgErroIdade;
-    if (fieldFocus == undefined || fieldFocus == null || fieldFocus == "") {
-      fieldFocus = "idadeInput";
-    }
-  }
-  if (!validarSenha(senhaInput.value)) {
-    msgErro += msgErroSenha;
-    if (fieldFocus == undefined || fieldFocus == null || fieldFocus == "") {
-      fieldFocus = "senhaInput";
-    }
-  }
-  if (!confereSenha(senhaInput.value, confereSenhaInput.value)) {
-    msgErro += msgErroConfereSenha;
-    if (fieldFocus == undefined || fieldFocus == null || fieldFocus == "") {
-      fieldFocus = "confereSenhaInput";
-    }
-  }
-  if (msgErro == "") {
+  });
+
+  if (msgErro === "") {
     alert("Dados enviados com sucesso!");
   } else {
-    alert(msgErro);
+    const focusField = fields.find((field) => field.input.id === fieldFocus);
     event.preventDefault();
-    if (fieldFocus == "usernameInput") {
-      usernameInput.focus();
-    } else if (fieldFocus == "emailInput") {
-      emailInput.focus();
-    } else if (fieldFocus == "idadeInput") {
-      idadeInput.value = "";
-      idadeInput.focus();
-    } else if (fieldFocus == "senhaInput") {
-      senhaInput.focus();
-    } else if (fieldFocus == "confereSenhaInput") {
-      confereSenhaInput.focus();
+    alert(msgErro);
+    if (focusField) {
+      focusField.input.focus();
     }
   }
-});
+}
 
-function validarUsername() {
-  const statusUsername = usernameInput.value.length > 5;
-  formatarCampo(
-    statusUsername,
-    usernameLabel,
-    usernameInput,
-    usernameHelper,
-    msgErroUsername
-  );
-  return statusUsername;
+function validarCampoGenerico(value, fieldName, validateFunction) {
+  const fieldIndex = encontrarIndiceDoCampo(fieldName);
+  const isValid = validateFunction(value);
+
+  formatarCampo(fieldIndex, isValid);
+  return isValid;
+}
+
+function validarUsername(username) {
+  return username.length > 5;
 }
 
 function validarEmail(email) {
   const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  const statusEmail = regexEmail.test(email);
-  formatarCampo(statusEmail, emailLabel, emailInput, emailHelper, msgErroEmail);
-  return statusEmail;
+  return regexEmail.test(email);
 }
 
 function validarIdade(idade) {
-  let statusIdade = true; // Campo idade é opcional e não foi preenchido
-  // Verifica se o campo idade foi preenchido
-  if (idade !== undefined && idade !== null && idade !== "") {
-    // Converte o valor para um número
-    idade = parseInt(idade);
-    // Verifica se o valor é um número inteiro e está dentro do intervalo desejado
-    statusIdade = !!(Number.isInteger(idade) && idade > 0 && idade < 150);
+  if (idade === undefined || idade === null || idade === "") {
+    return true;
   }
-  formatarCampo(statusIdade, idadeLabel, idadeInput, idadeHelper, msgErroIdade);
-  return statusIdade;
+
+  idade = parseInt(idade);
+  return !!(Number.isInteger(idade) && idade > 0 && idade < 150);
 }
 
 function validarSenha(password) {
-  let statusSenha;
-  // Verifica o comprimento mínimo (geralmente 8 caracteres ou mais)
-  if (password.length < 8) {
-    statusSenha = false;
-  }
-  // Verifica se há pelo menos uma letra maiúscula
-  if (!/[A-Z]/.test(password)) {
-    statusSenha = false;
-  }
-  // Verifica se há pelo menos uma letra minúscula
-  if (!/[a-z]/.test(password)) {
-    statusSenha = false;
-  }
-  // Verifica se há pelo menos um número
-  if (!/[0-9]/.test(password)) {
-    statusSenha = false;
-  }
-  // Verifica se há pelo menos um caractere especial (por exemplo, !@#$%^&*)
-  statusSenha = !!/[-!@#\\$%^&*]/.test(password);
+  const minLength = password.length >= 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+  const hasSpecialChar = /[-!@#$%^&*]/.test(password);
 
-  formatarCampo(statusSenha, senhaLabel, senhaInput, senhaHelper, msgErroSenha);
-  return statusSenha;
+  return minLength && hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
 }
 
-function confereSenha(senha, confereSenha) {
-  const statusConfereSenha = senha === confereSenha && senha !== "";
-  formatarCampo(
-    statusConfereSenha,
-    confereSenhaLabel,
-    confereSenhaInput,
-    confereSenhaHelper,
-    msgErroConfereSenha
-  );
-  return statusConfereSenha;
+function confereSenha(senha, confirmaSenha) {
+  return senha === confirmaSenha && senha !== "";
 }
 
-// Formatar o campo conforme o status da validação
-function formatarCampo(
-  status,
-  objetoLabel,
-  objetoInput,
-  objetoHelper,
-  mensagemAjuda
-) {
+function formatarCampo(indexField, status) {
+  const field = fields[indexField];
+  const { label, input, helper, errorMessage } = field;
+
   if (status) {
-    objetoLabel.classList.remove("required-popup");
-    objetoInput.classList.remove("error");
-    objetoInput.classList.add("correct");
-    objetoHelper.classList.remove("visible");
+    label.classList.remove("required-popup");
+    input.classList.remove("error");
+    input.classList.add("correct");
+    helper.classList.remove("visible");
   } else {
-    objetoInput.classList.remove("correct");
-    objetoInput.classList.add("error");
-    objetoHelper.classList.add("visible");
-    objetoHelper.innerHTML = mensagemAjuda;
+    input.classList.remove("correct");
+    input.classList.add("error");
+    helper.classList.add("visible");
+    helper.innerHTML = errorMessage;
   }
 }
 
-// Mostrar popup de campo obrigatório
-usernameInput.addEventListener("focus", () => {
-  usernameLabel.classList.add("required-popup");
-});
+fields.forEach((field) => {
+  // Adicionar tratamento de eventos 'focus' para campos obrigatórios
+  field.input.addEventListener("focus", () => {
+    if (field.requerido) {
+      field.label.classList.add("required-popup");
+    }
+  });
 
-emailInput.addEventListener("focus", () => {
-  emailLabel.classList.add("required-popup");
-});
-
-senhaInput.addEventListener("focus", () => {
-  senhaLabel.classList.add("required-popup");
-});
-
-confereSenhaInput.addEventListener("focus", () => {
-  confereSenhaLabel.classList.add("required-popup");
-});
-
-// Validar valor do input
-usernameInput.addEventListener("blur", (e) => {
-  validarUsername();
-});
-
-emailInput.addEventListener("blur", (e) => {
-  validarEmail(emailInput.value);
-});
-
-idadeInput.addEventListener("blur", (e) => {
-  validarIdade(e.target.value);
-});
-
-senhaInput.addEventListener("blur", (e) => {
-  validarSenha(e.target.value);
-});
-
-confereSenhaInput.addEventListener("blur", (e) => {
-  confereSenha(senhaInput.value, e.target.value);
+// Adicionar tratamento de eventos 'blur' para validação
+  field.input.addEventListener("blur", (e) => {
+    validarCampoGenerico(field.input.value, field.fieldName, field.validate);
+  });
 });
